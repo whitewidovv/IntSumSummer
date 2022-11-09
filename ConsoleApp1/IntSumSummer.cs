bacdfa2a2b97e6f0e9673776a7e2072b20aefdb8
whitewidovv <neilof1227@gmail.com>
2022-11-09 17:23:40 -0500

    Add files via upload

---
+++ ConsoleApp1/IntSumSummer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class IntSumSummer
+    {
+        //method stub inspired from https://stackoverflow.com/a/39342619
+        private static void GetCombinations<T>(List<T> set, List<List<T>> result)
+        {
+            for (int i = 0; i < set.Count; i++)
+            {
+                List<T> temp = new List<T>(set.Where((s, index) => index != i));
+
+                if (temp.Count > 0 && !result.Where(l => l.Count == temp.Count).Any(l => l.SequenceEqual(temp)))
+                {
+                    result.Add(temp);
+
+                    GetCombinations<T>(temp, result);
+                }
+            }
+        }
+        private List<List<int>> getSets(List<int> numList)
+        {
+            List<List<int>> sets = new List<List<int>>();
+
+            if (numList.Count <= 1)
+            {
+                return new List<List<int>>();
+            }
+            List<int> baseSet = new List<int>();
+            baseSet.AddRange(numList);
+            sets.Add(baseSet);// ie, add {1,2,3,4} if we're passed {1,2,3,4} because GetCombinations doesn't return the base set
+            GetCombinations<int>(numList, sets);
+            
+            return sets;
+        }
+
+        public int GetSum(List<int> numList)
+        {
+            var sets = getSets(numList);
+            List<int> setSums = new List<int>();
+
+            foreach (var set in sets)
+            {
+                if (set.Count > 1)
+                {
+                    if (!setSums.Contains(set.Sum()))
+                    {
+                        setSums.Add(set.Sum());
+                    }
+                }
+            }
+
+            return setSums.Sum();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ConsoleApp1;
+
+
+IntSumSummer summer = new IntSumSummer();
+
+List<int> testSet1 = new List<int>() { 1 }; // 0
+List<int> testSet2 = new List<int>() { 1, 2}; // 3
+List<int> testSet3 = new List<int>() { 5, 2}; // 7
+List<int> testSet4 = new List<int>() { 1, 2, 3}; // 18
+List<int> testSet5 = new List<int>() { 1, 2, 3, 4}; // 52
+List<int> testSet6 = new List<int>() { 1, 2, 3, 4, 5}; // 117
+List<int> testSet7 = new List<int>() { 3, 2, 8, 41, 5, 6}; // 1248
+
+Console.WriteLine("summer.GetSum(testSet1): " + summer.GetSum(testSet1));
+Console.WriteLine("summer.GetSum(testSet2): " + summer.GetSum(testSet2));
+Console.WriteLine("summer.GetSum(testSet3): " + summer.GetSum(testSet3));
+Console.WriteLine("summer.GetSum(testSet4): " + summer.GetSum(testSet4));
+Console.WriteLine("summer.GetSum(testSet5): " + summer.GetSum(testSet5));
+Console.WriteLine("summer.GetSum(testSet6): " + summer.GetSum(testSet6));
+Console.WriteLine("summer.GetSum(testSet7): " + summer.GetSum(testSet7));
